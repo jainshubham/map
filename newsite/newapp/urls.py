e8b2c801f9fc80fe19e72ab5147705ddc47900fa
Shubham Jain <shubham2006@gmail.com>
2014-12-24 01:44:03 +0530

    Basic map app working

--- newsite/newapp/urls.py
+++ newsite/newapp/urls.py
@@ -1,25 +1,23 @@
+
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
-    # Examples:
-urlpatterns = patterns('newapp.views',
-    # url(r'^$', 'newsite.views.home', name='home'),
-    # url(r'^newsite/', include('newsite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^$', 'index'),
-    url(r'^(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+urlpatterns = patterns('',
+    url(r'^$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html')),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html')),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'newapp.views.vote'),
 )
 
-
-urlpatterns += patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-)
--- newsite/newapp/views.py
+++ newsite/newapp/views.py
@@ -1,11 +1,13 @@
 # Create your views here.
 from newapp.models import Poll
 from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
+from newapp.models import Choice, Poll
 
 from django.template import RequestContext
-
+from django.core.urlresolvers import reverse
 from django.http import Http404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 def index(request):
@@ -16,8 +18,26 @@ def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     return render_to_response('polls/detail.html', {'poll': p},
                                context_instance=RequestContext(request))
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('newapp.views.results', args=(p.id,)))
+
+
+def results(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
+
